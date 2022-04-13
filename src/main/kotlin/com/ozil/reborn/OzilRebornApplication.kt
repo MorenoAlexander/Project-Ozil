736d6de5170736b28977ba687af3e8b1ac80fc63
morenoalexander <am@alexandermoreno.com>
2022-04-13 09:13:11 -0500

    initial commit

---
+++ src/main/kotlin/com/ozil/reborn/OzilRebornApplication.kt
@@ -0,0 +1,11 @@
+package com.ozil.reborn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OzilRebornApplication
+
+fun main(args: Array<String>) {
+    runApplication<OzilRebornApplication>(*args)
+}
---
+++ src/test/kotlin/com/ozil/reborn/OzilRebornApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ozil.reborn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OzilRebornApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
